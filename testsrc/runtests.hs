827118622430f858afdf4dba45414ba83e087daa
segfault <s9gf4ult@gmail.com>
2013-08-08 23:19:34 +0600

    * affected rows added back

--- testsrc/runtests.hs
+++ testsrc/runtests.hs
@@ -8,35 +8,39 @@
 module Runtests where
 
 import Control.Exception (bracket)
+import Data.Int
+import Data.List (genericLength)
 import Database.HDBI
 import Database.HDBI.PostgreSQL
 import Database.HDBI.Tests
 import System.Environment
 import System.Exit
 import Test.Framework
+import Test.Framework.Providers.QuickCheck2
+import Test.QuickCheck
+import Test.QuickCheck.Assertions
 import qualified Data.Text.Lazy as TL
+import qualified Test.QuickCheck.Monadic as QM
   
   
 
--- testAffectedRows :: PostgreConnection -> [Int32] -> Property
--- testAffectedRows c is = QM.monadicIO $ do
---   res <- QM.run $ withTransaction c $ do
---     runRaw c "drop table if exists table1"
---     runRaw c "create table table1 (val bigint)"
---     runMany c "insert into table1(val) values (?)" $ map ((:[]) . toSql) is
-
---     s2 <- prepare c "update table1 set val=10"
---     executeRaw s2
---     res <- affectedRows s2
---     finish s2
+testAffectedRows :: PostgreConnection -> [Int32] -> Property
+testAffectedRows c is = QM.monadicIO $ do
+  res <- QM.run $ withTransaction c $ do
+    runRaw c "drop table if exists table2"
+    runRaw c "create table table2 (val bigint)"
+    runMany c "insert into table2(val) values (?)" $ map ((:[]) . toSql) is
+    withStatement c "update table2 set val=10" $ \s -> do
+      executeRaw s
+      pgAffectedRows s
   
---     return res
---   QM.stop $ res ?== (genericLength is)
+  QM.stop $ res ?== (genericLength is)
            
--- testG2 :: PostgreConnection -> Test
--- testG2 c = testGroup "Auxiliary functions"
---            [ testProperty "affectedRows" $ testAffectedRows c ]
+pgTests :: PostgreConnection -> Test
+pgTests c = testGroup "Auxiliary functions"
+           [ testProperty "affectedRows" $ testAffectedRows c ]
 
+fields :: TestFieldTypes
 fields = TestFieldTypes
          { tfDecimal = "decimal(600,300)"
          , tfInteger = "decimal(300,0)"
@@ -60,7 +64,9 @@ main = do
     (conn:args) -> bracket
                    (connectPostgreSQL $ TL.pack conn)
                    disconnect
-                   $ \c -> (flip defaultMainWithArgs) args [ allTests fields c ]
+                   $ \c -> (flip defaultMainWithArgs) args [ allTests fields c
+                                                           ,pgTests c
+                                                           ]
 
     _ -> do
       mapM_ putStrLn [ "Need at least one argument as connection string"
